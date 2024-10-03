cab3fd39376b1ff116a80247fa58892e8fc05736
Sepehr Mohamadpor <sepehr1628@gmail.com>
2024-10-03 13:41:57 +0330

    feature/ json server coorectly added now trying to filter

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import Filter from "../features/products/Filter";
 import BreadCrumb from "../ui/BreadCrumb";
 import Results from "../ui/Results";
+import { useLocation, useNavigate } from "react-router";
 
 function Market() {
   const [showFilter, setShowFilter] = useState(false);
-  const [filteredData, setFilteredData] = useState();
   const [filters, setFilters] = useState({
     brand: {
       apple: false,
@@ -28,48 +28,13 @@ function Market() {
     },
   });
 
-  useEffect(
-    function () {
-      const filteredProducts = async () => {
-        const response = await fetch("./../../db.json");
-        const data = await response.json();
-        const filteredData = data.laptops.filter((product) => {
-          if (filters.brand.apple && product.brand !== "Apple") return false;
-          if (filters.brand.asus && product.brand !== "Asus") return false;
-          if (filters.brand.hp && product.brand !== "Hp") return false;
-          if (filters.brand.acer && product.brand !== "Acer") return false;
-          if (filters.brand.lenovo && product.brand !== "Lenovo") return false;
-          if (filters.price.min && product.price < filters.price.min)
-            return false;
-          if (filters.price.max && product.price > filters.price.max)
-            return false;
-          if (filters.cpu.intel && product.cpu !== "Intel") return false;
-          if (filters.cpu.amd && product.cpu !== "Amd") return false;
-          if (filters.type.laptop && product.type !== "laptop") return false;
-          if (filters.type.desktop && product.type !== "desktop") return false;
-          return true;
-        });
-        console.log(filteredData);
-
-        return filteredData;
-      };
-      filteredProducts().then;
-      (data) => setFilteredData(data);
-    },
-    [
-      filters.brand.apple,
-      filters.brand.asus,
-      filters.brand.hp,
-      filters.brand.acer,
-      filters.brand.lenovo,
-      filters.price.min,
-      filters.price.max,
-      filters.cpu.intel,
-      filters.cpu.amd,
-      filters.type.laptop,
-      filters.type.desktop,
-    ]
-  );
+  useEffect(function () {
+    async function fetchProducts() {
+      const response = await fetch(`Http://localhost:5000/laptops`);
+      const data = await response.json();
+    }
+    fetchProducts();
+  }, []);
 
   return (
     <section>
@@ -80,7 +45,7 @@ function Market() {
           filters={filters}
           setFilters={setFilters}
         />
-        <Results filteredData={filteredData} setShowFilter={setShowFilter} />
+        <Results setShowFilter={setShowFilter} />
       </div>
     </section>
   );
